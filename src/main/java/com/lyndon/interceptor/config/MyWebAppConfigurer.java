9bccb45246d4280c44ba63e6f6c5a3e6cc75bf11
EZNLZHI <lindong.zhao@ericsson.com>
2017-12-27 13:44:45 +0800

    拦截器演示Demo

---
+++ src/main/java/com/lyndon/interceptor/config/MyWebAppConfigurer.java
@@ -0,0 +1,28 @@
+package com.lyndon.interceptor.config;
+
+import com.lyndon.interceptor.interceptors.MyInterceptor1;
+import com.lyndon.interceptor.interceptors.MyInterceptor2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Author: eznlzhi
+ * @Email: lindong.zhao@ericsson.com
+ * @Date: 12/27/2017
+ */
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private MyInterceptor1 myInterceptor1;
+    @Autowired
+    private MyInterceptor2 myInterceptor2;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor1).addPathPatterns("/**");
+        registry.addInterceptor(myInterceptor2).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+}
---
+++ src/main/java/com/lyndon/interceptor/web/TargetController.java
@@ -0,0 +1,18 @@
+package com.lyndon.interceptor.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: eznlzhi
+ * @Email: lindong.zhao@ericsson.com
+ * @Date: 12/27/2017
+ */
+@RestController
+public class TargetController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello MyInterceptor !";
+    }
+}
